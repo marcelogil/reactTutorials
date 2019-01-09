0d3bb3b1a03fca668adf2ddee09d3a1bb631caa4
Gil <marcelogil.mail@gmail.com>
2019-01-09 15:09:02 +0100

    Small changes

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 
 import App from './components/App'
 import reducers from './reducers'
 
+/* 
+const enhancers = compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+ */
 const store = createStore(reducers, applyMiddleware(thunk))
 
 ReactDOM.render(
--- songs/src/index.js
+++ songs/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 
 import App from './components/App'
 import reducers from './reducers'
 
+const enhancers = compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+const store = createStore(reducers, enhancers)
+
 ReactDOM.render(
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector('#root')
