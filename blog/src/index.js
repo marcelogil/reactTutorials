6c6feb1c912e81db9df6c5435bc3e1de09d04e14
Gil <marcelogil.mail@gmail.com>
2019-01-09 15:24:38 +0100

    Added configuration for Redux devtools

--- blog/src/index.js
+++ blog/src/index.js
@@ -7,12 +7,11 @@ import thunk from 'redux-thunk'
 import App from './components/App'
 import reducers from './reducers'
 
-/* 
 const enhancers = compose(
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
- */
-const store = createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers, enhancers)
 
 ReactDOM.render(
     <Provider store={store}>
